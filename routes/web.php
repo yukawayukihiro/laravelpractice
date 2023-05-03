5210e897351020d6a4786aa379d2472bb412e84c
yukawa <metalg9123@i.softbank.jp>
2023-05-03 23:50:50 +0900

    2-1ルーティング

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('hello/{msg?}',function ($msg = 'no message.') {
+    $html = <<<EOF
+    <html>
+    <head>
+    <title>Hello</title>
+    <style>
+    body {font-size: 16px; color: #999;}
+    h1 {font-size: 100px; text-align: right; color: #eee; margin: -40px 0 px -50px 0px;}
+    </style>
+    <body>
+        <h1>Hello</h1>
+        <p>{$msg}</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+    </html>
+    EOF;
+
+    return $html;
+});
