456137c2f23783d39282b04e62ce409507311a52
yukawa <metalg9123@i.softbank.jp>
2023-05-07 20:01:05 +0900

    レイアウトの作成

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index () {
-        $data = ['one','two','three','four','five'];
+        $data = [
+            ['name'=>'山田たろう','mail'=>'taro@yamada'],
+            ['name'=>'田中はなこ','mail'=>'hanako@flower'],
+            ['name'=>'鈴木さちこ','mail'=>'sachiko@happy']
+        ];
         return view('hello.index',['data'=>$data]);
     }
 }
